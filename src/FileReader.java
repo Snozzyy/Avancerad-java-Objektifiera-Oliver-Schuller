750cbbd7de9927d53a1e8bff3e715360299c0559
Snozzyy <oliverschuller@hotmail.com>
2023-11-30 21:40:59 +0100

    Reads and displays CSV-files

--- src/FileReader.java
+++ src/FileReader.java
@@ -1,12 +1,74 @@
+import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
 import java.io.BufferedReader;
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class FileReader {
+
+    private static String file;
+    private static String extension;
+    static String[] header;
+    static Object[][] data;
 
     public FileReader() {
 
     }
 
-    public static void readCSV(String path) {
+    public static String openExplorer() {
+        // Open in working directory
+        JFileChooser fileChooser = new JFileChooser();
+        fileChooser.setCurrentDirectory(new File("."));
+        int response = fileChooser.showOpenDialog(null);
 
+        // Get absolute path to chosen file
+        if (response == JFileChooser.APPROVE_OPTION) {
+            File path = new File(fileChooser.getSelectedFile().getAbsolutePath());
+            file = String.valueOf(path);
+            return file;
+        }
+        return null;
+    }
+
+    // Reads the CSV-file and returns TableModel containing header and rows from file
+    public static void readCSV() {
+        try {
+            BufferedReader br = new BufferedReader(new java.io.FileReader(file));
+            ArrayList<String> list = new ArrayList<>();
+            String str = "";
+
+            while ((str = br.readLine()) != null){
+                list.add(str);
+                //System.out.println(str);
+            }
+
+            // Create 2d array representing table row and column
+            int n = list.get(0).split(",").length;
+            data = new Object[list.size()][n];
+
+            for (int i = 0; i < list.size(); i++) {
+                data[i] = list.get(i).split(",");
+            }
+            // Stop read the file
+            br.close();
+
+            // Get the header of CSV-file
+            header = list.get(0).split(",");
+
+            GUI.setTableModel(data, header);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    // Get which type of file is chosen
+    public static String getExtension() {
+        int i = file.lastIndexOf(".");
+        if (i > 0) {
+            extension = file.substring(i + 1);
+        }
+        return extension;
     }
 }
--- src/GUI.java
+++ src/GUI.java
@@ -1,35 +1,75 @@
 import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.table.JTableHeader;
+import javax.swing.table.TableColumn;
+import javax.swing.table.TableColumnModel;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.io.File;
+import java.util.Arrays;
 
 public class GUI  extends JFrame implements ActionListener {
 
-    JButton button;
+    private JTextField showPath;
+    private JButton chooseFile;
+    private JButton showFile;
+    private JPanel tablePanel;
+    private static JTable table;
+
+
     public GUI() {
-        super("Test");
+        super("Objektivering");
         setSize(1080, 640);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
 
-        button = new JButton("Select file");
-        button.addActionListener(this);
+        JPanel panel = new JPanel(new BorderLayout());
+        JPanel footerPanel = new JPanel();
+        tablePanel = new JPanel();
 
-        // tableview
+        table = new JTable();
+        table.setAutoCreateRowSorter(true);
 
-        add(button);
+        ScrollPane scrollPane = new ScrollPane();
+        scrollPane.add(table);
+        scrollPane.setSize(1000, 520);
+        tablePanel.add(scrollPane);
+
+        showPath = new JTextField(30);
+        footerPanel.add(showPath);
+
+        chooseFile = new JButton("Choose file");
+        chooseFile.addActionListener(this);
+        footerPanel.add(chooseFile);
+
+        showFile = new JButton("Show in table");
+        showFile.addActionListener(this);
+        footerPanel.add(showFile);
+
+        panel.add(tablePanel, BorderLayout.CENTER);
+        panel.add(footerPanel, BorderLayout.SOUTH);
+
+        add(panel);
+
         setVisible(true);
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        if (e.getSource() == button) {
-            JFileChooser fileChooser = new JFileChooser();
-            fileChooser.setCurrentDirectory(new File("."));
-            int response = fileChooser.showOpenDialog(null);
+        if (e.getSource() == chooseFile)
+            showPath.setText(FileReader.openExplorer());
+        else if (e.getSource() == showFile);
+            if (FileReader.getExtension().equals("csv"))
+                FileReader.readCSV();
 
-            if (response == JFileChooser.APPROVE_OPTION) {
-                File path = new File(fileChooser.getSelectedFile().getAbsolutePath());
-            }
+    }
+
+    public static void setTableModel(Object[][] data, String[] header) {
+
+        DefaultTableModel tableModel = new DefaultTableModel(header, 0);
+        for (int i = 1; i < data.length; i++) {
+            tableModel.addRow(data[i]);
         }
+        table.setModel(tableModel);
+        
     }
 }
